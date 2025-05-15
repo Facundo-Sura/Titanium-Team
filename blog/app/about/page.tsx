4c694e5f7e9ccd2202ab14bc50ea15abe842d77f
facundo sura <facundomesura@gmail.com>
2025-05-15 02:28:12 -0300

    cambios en navbar y about además elimine la pagina de contacto

--- blog/app/about/page.tsx
+++ blog/app/about/page.tsx
@@ -1,7 +1,10 @@
 export default function About() {
     return (
-        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-            <h1>Nosotros</h1>
+        <div className="bg-[url(/about.jpg)] bg-center bg-fixed bg-no-repeat bg-cover grid grid-rows-[20px_1fr_20px] items-center justify-items-center w-full min-h-screen gap-16 sm:p-20 ">
+            <main className="bg-black w-full ">
+                <h1 className="text-center">Nosotros</h1>
+                <p className="text-center">Nuestro dojo esta ubicado en la ciudad de Alta Gracia provicia de Cordoba en Argentina</p>
+            </main>
         </div>
     )
 }
--- blog/components/Navbar.tsx
+++ blog/components/Navbar.tsx
@@ -4,15 +4,14 @@ import Image from "next/image"
 
 const Navbar = () => {
     return (
-        <header className="fixed w-full flex flex-wrap justify-around items-center">
-            <Link href="/">
-                <Image src="/logo.jpg" width={50} height={50} alt="Titanium Team" />
-            </Link>
+        <header className="fixed bg-linear-to-b from-blue-950 via-black to-red-600 w-full flex flex-wrap justify-around items-center">
             <Link href="/about">Nosotros</Link>
             <Link href="/pupils">Alumnos</Link>
+            <Link href="/">
+                <Image src="/logo.jpg" width={50} height={50} alt="Titanium Team" className="rounded-xl" />
+            </Link>
             <Link href="/trainings">Entrenamientos</Link>
             <Link href="/combats">Combates</Link>
-            <Link href="/contact">Contactanos</Link>
         </header>
     )
 }
