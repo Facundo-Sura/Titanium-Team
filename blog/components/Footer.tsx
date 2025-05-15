0acf50067592aaab4e632d3b6c1af60f7570d0f7
facundomesura <facundomesura@gmail.com>
2025-05-15 04:20:32 +0000

    páginas agregadas

---
+++ blog/components/Footer.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import Link from "next/link";
+import Image from "next/image";
+
+const Footer = () => {
+    return (
+        <footer></footer>
+    )
+};
+
+export default Footer;
--- blog/components/Navbar.tsx
+++ blog/components/Navbar.tsx
@@ -4,7 +4,16 @@ import Image from "next/image"
 
 const Navbar = () => {
     return (
-        <header></header>
+        <header className="fixed w-full flex flex-wrap justify-around items-center">
+            <Link href="/">
+                <Image src="/logo.jpg" width={50} height={50} alt="Titanium Team" />
+            </Link>
+            <Link href="/about">Nosotros</Link>
+            <Link href="/pupils">Alumnos</Link>
+            <Link href="/trainings">Entrenamientos</Link>
+            <Link href="/combats">Combates</Link>
+            <Link href="/contact">Contactanos</Link>
+        </header>
     )
 }
 
